4e694e6d54751d8aeb7290164decca71142d15ca
GEMMA BUCKLAND <b7010126@hallam.shu.ac.uk>
2018-04-26 17:29:43 +0100

    Location of media txt file
    i have changed the location of the media txt file this is because it didnt seem to work and understand so hopefully this will work correctly now.

--- Juke_Box/Form3.Designer.cs
+++ Juke_Box/Form3.Designer.cs
@@ -53,6 +53,7 @@
             this.textBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(207, 22);
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(187, 27);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "My Juke Box v1.0";
